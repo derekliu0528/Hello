565a7c7d5b7e74f60073254c4248148d9b3a3d80
Derek <derek.liu.0528@gmail.com>
2018-10-12 09:41:29 +0800

    Update

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,15 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("view") { req -> Future<View> in
+        return try req.view().render("welcome")
+    }
+    
+    router.get("bonus") { req -> Future<View> in
+        let data = ["name": "Ethan", "age": "26"]
+        return try req.view().render("whoami", data)
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
